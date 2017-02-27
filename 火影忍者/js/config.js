8f4da754d8ac8920aec85eb10335eb8e074169f0
MyleneHuang <1161199450@qq.com>
2017-02-27 23:26:24 +0800

    Game
    a game about Hokage Ninjia

---
+++ 火影忍者/js/config.js
@@ -0,0 +1,8 @@
+/* 
+* @Author: Administrator
+* @Date:   2017-02-26 19:01:55
+* @Last Modified by:   Administrator
+* @Last Modified time: 2017-02-26 19:05:32
+*/
+var url = 'http://192.168.1.104/laravel/laravel/public/';
+
